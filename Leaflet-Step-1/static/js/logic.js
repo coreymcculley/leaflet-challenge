1c1867ebaf6dac8b50796bca5e583e1a9112b127
coreymcculley <corey.mcculley@gmail.com>
2020-12-01 15:26:08 -0600

    working on function to make map for part 1

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,25 +1,32 @@
 
-// Creating map object
-var myMap = L.map("map", {
-  center: [40.7128, -74.0059],
-  zoom: 11
-});
+// // Creating map object
+// var myMap = L.map("map", {
+//   center: [40.7128, -74.0059],
+//   zoom: 11
+// });
 
-// Adding tile layer
-L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-  maxZoom: 18,
-  id: "streets-v11",
-  accessToken: API_KEY
-}).addTo(map);
+// // Adding tile layer
+// L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+//   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+//   maxZoom: 18,
+//   id: "streets-v11",
+//   accessToken: API_KEY
+// }).addTo(map);
 
 // Store our API endpoint inside queryUrl
-var queryUrl =
-  "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+var queryUrl =  "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 console.log(queryUrl);
 
-// Grabbing our GeoJSON data..
+// 
 d3.json(queryUrl).then(function(data) {
   // Creating a GeoJSON layer with the retrieved data
-  L.geoJson(data).addTo(myMap);
+  createMap(data.features);
 });
+
+//Write function to take input json data and output a leaflet map with buttons
+function createMap (geoData){
+
+
+  
+
+};
