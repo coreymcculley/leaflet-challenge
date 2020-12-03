434d7d7f8c463ba7fdeb7af39ba703f59a9d34b9
coreymcculley <corey.mcculley@gmail.com>
2020-12-03 09:40:58 -0600

    Finished part1 of the HW

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,55 +1,107 @@
-var myMap = L.map("mapid", {
-  center: [29.76, -95.37],
-  zoom: 11
+// URL for the earthquake geojson data
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
+
+// Perform a GET request to the query URL
+d3.json(queryUrl, function (data) {
+  // Send json data to quakeMap function 
+  quakeMap(data.features);
 });
 
-L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-  maxZoom: 18,
-  id: "streets-v11",
-  accessToken: API_KEY
-}).addTo(myMap);
+function quakeMap(d) {
+  // Loop through locations and markers elements, add popups
+  EarthquakeMarkers = d.map((feature) =>
+    L.circleMarker(
+      [feature.geometry.coordinates[1], feature.geometry.coordinates[0]],
+      {
+        radius: magAdjust(feature.properties.mag),
+        stroke: true,
+        color: "black",
+        opacity: 1,
+        weight: 0.5,
+        fill: true,
+        fillColor: magColor(feature.properties.mag),
+        fillOpacity: 0.9,
+      }
+    ).bindPopup(
+      "<h2> Magnitude: " +
+        feature.properties.mag +
+        "</h1><hr><h2>" +
+        feature.properties.place +
+        "</h2><hr><p>" +
+        new Date(feature.properties.time) +
+        "</p>"
+    )
+  );
 
-// // Store our API endpoint inside queryUrl
-// var queryUrl =
-//   "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
-// console.log(queryUrl);
+  // Add the earthquakes layer to a marker cluster group.
+  var earthquakes = L.layerGroup(EarthquakeMarkers);
 
-// //
-// d3.json(queryUrl, function (data) {
-//   // Creating a GeoJSON layer with the retrieved data
-//   createMap(data.features);
-// });
+  var mags = d.map((d) => magAdjust(+d.properties.mag));
 
-// //Write function to take input json data and output a leaflet map with buttons
-// function createMap(geoData) {
-//   //Loop and create markers for the earthquake geo data
-//   geoMarkers = geoData.map((feature) =>
-//     L.circleMarker(
-//       [feature.geometry.coordinates[1], feature.geometry.coordinates[0]],
-//       {
-//         radius: feature.properties.mag, //set circle size to magnitude from geo data
-//         stroke: true,
-//         color: "black",
-//         opacity: 0.5,
-//         weight: 0.5,
-//         fill: true,
-//         fillColor: "blue", //magColor(feature.properties.mag),
-//         fillOpacity: 0.9,
-//       }
-//       // ).bindPopup(
-//       //   "<h1> Magnitude : " +
-//       //     feature.properties.mag +
-//       //     "</h1><hr><h3>" +
-//       //     feature.properties.place +
-//       //     "</h3><hr><p>" +
-//       //     new Date(feature.properties.time) +
-//       //     "</p>"
-//       // )
-//     )
-//   );
+  // Define streetmap layer
+  var streetmap = L.tileLayer(
+    "https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+    {
+      attribution:
+        'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: "streets-v11",
+      accessToken: API_KEY,
+    }
+  );
 
+  // Create our map, giving it the streetmap and earthquakes layers to display on load
+  var myMap = L.map("map", {
+    center: [29.76, -95.37],
+    zoom: 5.0,
+    layers: [streetmap, earthquakes],
+  });
 
-//   // Creating map object
-  
-// }
+  // Create and Add Legend
+  var legend = L.control({ position: "bottomleft" });
+  (labels = ["<strong>Magnitude</strong>"]),
+    (legend.onAdd = function (myMap) {
+      var div = L.DomUtil.create("div", "legend");
+
+      div.innerHTML = [
+        "<k class='mag2'></k><span><2</span><br>",
+        "<k class='mag3'></k><span>2-3</span><br>",
+        "<k class='mag4'></k><span>3-4</span><br>",
+        "<k class='mag5'></k><span>4-5</span><br>",
+        "<k class='mag5up'></k><span>5+</span><br>",
+      ].join("");
+      return div;
+    });
+
+  legend.addTo(myMap);
+}
+
+//Define the color scheme for the circles
+function magColor(mag) {
+  var color = "";
+  if (mag <= 2) {
+    color = "#788ef5";
+  } 
+  else if (mag <= 3) {
+    color = "#9dceef";
+  } 
+  else if (mag <= 4) {
+    color = "#ffffe0";
+  } 
+  else if (mag <= 5) {
+    color = "#f4777f";
+  } 
+  else {
+    color = "#93003a";
+  }
+
+  return color;
+}
+
+//Scale the circle size to the magnitude (X^2) and define minimum size for <2 magnitude
+function magAdjust(mag) {
+  if (mag <= 1) {
+    return 2;
+  }
+  return (mag * mag );
+}
